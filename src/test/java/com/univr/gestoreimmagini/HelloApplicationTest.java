a74f92c9e9a573425d950f06b3b6d3d39f78fa13
Floppy-disko <mat4disco@gmail.com>
2022-08-26 14:39:39 +0200

    Aggiunto il framework ditesting TestFX ed un esempio di test per vedere se funziona a tutti

---
+++ src/test/java/com/univr/gestoreimmagini/HelloApplicationTest.java
@@ -0,0 +1,71 @@
+package com.univr.gestoreimmagini;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.testfx.api.FxAssert;
+import org.testfx.api.FxRobot;
+import org.testfx.framework.junit5.ApplicationExtension;
+import org.testfx.framework.junit5.Start;
+import org.testfx.matcher.control.LabeledMatchers;
+
+import java.io.IOException;
+
+// usare testfx non funziona con i moduli
+@ExtendWith(ApplicationExtension.class)
+class HelloApplicationTest {
+
+    @Start
+    public void Start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /**
+     * @param robot - Will be injected by the test runner.
+     */
+    @Test
+    void should_contain_button_with_text(FxRobot robot) {
+        // or (lookup by css id):
+        FxAssert.verifyThat("#helloButton", LabeledMatchers.hasText("Hello!"));
+        // or (lookup by css class):
+        FxAssert.verifyThat(".button", LabeledMatchers.hasText("Hello!"));
+    }
+
+    @BeforeAll
+    static void setup() {
+        System.out.println("@BeforeAll - executes once before all test methods in this class");
+    }
+
+    @BeforeEach
+    void init() {
+        System.out.println("@BeforeEach - executes before each test method in this class");
+    }
+
+
+    @DisplayName("Single test successful")
+    @Test
+    void testSingleSuccessTest() {
+        System.out.println("Success");
+    }
+
+    @Test
+    @Disabled("Not implemented yet")
+    void testShowSomething() {
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.out.println("@AfterEach - executed after each test method.");
+    }
+
+    @AfterAll
+    static void done() {
+        System.out.println("@AfterAll - executed after all test methods.");
+    }
+}
